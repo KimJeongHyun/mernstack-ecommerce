720541dc0d03a3bd5b5b4e689de6ba555e51f756
KimJeongHyun <ptr1129@hanmail.net>
2021-10-20 21:43:11 +0900

    video insert and wrapping div applied

--- client/src/component/LandingPage/NewLandingPage.js
+++ client/src/component/LandingPage/NewLandingPage.js
@@ -1,6 +1,7 @@
 import React, {useState,useRef,useEffect} from 'react'
 import { NavSideBar } from '../NavBar/NavSideBar'
 import {gsap} from 'gsap'
+import {NavBar} from '../NavBar/NavBar'
 
 import '../../css/style.css'
 
@@ -10,9 +11,10 @@ function NewLandingPage(props){
 
     const boxRef = useRef();
     const textRef = useRef();
+    const videoURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
 
     useEffect(async ()=>{
-        await gsap.to(boxRef.current,{opacity:1,duration:2})
+        await gsap.to(boxRef.current,{opacity:0.7,duration:1})
         setBoxReady(true)
     },[])
 
@@ -25,8 +27,24 @@ function NewLandingPage(props){
     return(
         <>
         <div className="contentArea" >
-            <img ref={boxRef} style={{opacity:'0'}} src="images/Coor.jpg" alt="Initial View"></img>
-            <p ref={textRef} style={{opacity:'0',position:'absolute',left:'10%', bottom:'50%', color:'white'}}> 당신을 위한 최고의 선택 </p>
+            <div>
+                <video id="background-video" loop autoPlay muted>
+                    <source src={videoURL} type="video/mp4"/>    
+                </video>
+            </div>
+            <div id="videoWrapping" ref={boxRef}>
+                
+            </div>
+            <div style={{width:'100%',height:'500px', backgroundColor:'red'}}>
+                hi!
+            </div>
+            <div style={{width:'100%',height:'500px', backgroundColor:'red'}}>
+                hi!
+            </div>
+            <div style={{width:'100%',height:'500px', backgroundColor:'red'}}>
+                hi!
+            </div>
+            
         </div>
         </>
         
