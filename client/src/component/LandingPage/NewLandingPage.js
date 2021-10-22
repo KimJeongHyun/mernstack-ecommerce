fa0e8832b08943270400dabd25be4fea7c65818f
KimJeongHyun <ptr1129@hanmail.net>
2021-10-23 01:55:36 +0900

    newLandingPage image slider applied

--- client/src/component/LandingPage/NewLandingPage.js
+++ client/src/component/LandingPage/NewLandingPage.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux'
 import {gsap} from 'gsap'
 import { NewNavBar } from '../NavBar/NewNavBar'
 import { getClothes } from '../../_actions/user_action'
+import {BsFillArrowLeftSquareFill, BsFillArrowRightSquareFill} from 'react-icons/bs'
+
 
 import '../../css/style.css'
 import TopBtn from '../TopBtn/TopBtn'
@@ -13,7 +15,6 @@ function NewLandingPage(props){
     const [clothMap,setClothMap] = useState([]);
     const [imgLoc, setImgLoc] = useState(2)
     const [slideReady,setSlideReady] = useState(false);
-    const [slideInterval, setSlideInterval] = useState('');
 
     const dispatch = useDispatch();
 
@@ -22,6 +23,7 @@ function NewLandingPage(props){
     const textRef2 = useRef();
     const textRef3 = useRef();
     const imageRef = useRef();
+    const tickRef = useRef();
 
     const videoURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
 
@@ -42,35 +44,98 @@ function NewLandingPage(props){
             await gsap.to(textRef3.current,{opacity:1,duration:1})
             await gsap.to('.wrappingText',{right:'20%',duration:1})
             await gsap.to('.wrappingImage',{opacity:1})
+            document.getElementById('preventOnClick').remove();
             setSlideReady(true)
-            setSlideInterval(()=>{
-
-            })
         }
         worker();
         
     },[])
-
-    useEffect(()=>{
-
-        if (slideReady){
-            function tick(){
-                if (imgLoc+1<=clothMap.length){
-                    return setTimeout(()=>setImgLoc(imgLoc+1),1000)
-                }else{
-                    return setTimeout(()=>setImgLoc(2),1000)
+    /*const tick = () =>{
+        if (imgLoc+1<=clothMap.length){
+            return setTimeout(()=>{
+                if (imageRef.current!=null){
+                    setImgLoc(imgLoc+1)
                 }
                 
+            },4000)
+        }else{
+            return setTimeout(async ()=>{
+                if (imageRef.current!=null){
+                    setImgLoc(2)
+                }                
+            },4000)
+        }
+    }*/
+    useEffect(()=>{
+        if (slideReady){
+            clearTimeout(tickRef.current)
+            if (imgLoc+1<=clothMap.length){
+                tickRef.current = setTimeout(()=>{
+                    if (imageRef.current!=null){
+                        setImgLoc(imgLoc+1)
+                    }
+                    
+                },4000)
+            }else{
+                tickRef.current = setTimeout(async ()=>{
+                    if (imageRef.current!=null){
+                        setImgLoc(2)
+                    }                
+                },4000)
             }
-            tick();
-            return ()=>clearTimeout(tick);
+            
+            //tick();
+            return ()=>clearTimeout(tickRef.current);
         }
     },[slideReady,imgLoc])
 
     const productDetail = (event) =>{
         event.preventDefault();
-        
+        clearTimeout(tickRef.current);
+        props.history.push({
+            pathname:"/ProductDetail/"+imgLoc
+        })
     }
+
+    const buttonAppear = (event) =>{
+        event.preventDefault();
+        const prevBtn = document.getElementById('prevBtn');
+        const nextBtn = document.getElementById('nextBtn');
+        gsap.to(prevBtn,{opacity:1})
+        gsap.to(nextBtn,{opacity:1})
+    }
+
+    const buttonDisappear = (event) =>{
+        event.preventDefault();
+        const prevBtn = document.getElementById('prevBtn');
+        const nextBtn = document.getElementById('nextBtn');
+        gsap.to(prevBtn,{opacity:0})
+        gsap.to(nextBtn,{opacity:0})
+    }
+
+    const nextBtnClick = (event) =>{
+        event.preventDefault();
+        clearTimeout(tickRef.current);
+        if (imgLoc+1<=clothMap.length){
+                setImgLoc(imgLoc+1)
+        }else{
+            setImgLoc(2)
+        }
+    }
+
+    const prevBtnClick = (event) =>{
+        event.preventDefault();
+        clearTimeout(tickRef.current);
+        if (imgLoc-1>=2){
+                setImgLoc(imgLoc-1)
+        }else{
+            setImgLoc(clothMap.length)
+        }
+    }
+
+    useEffect(()=>{
+        console.log(imgLoc);
+    },[imgLoc])
 
     return(
         <>
@@ -92,12 +157,17 @@ function NewLandingPage(props){
                     <p>Feeling empathy on your life</p>
                 </div>
             </div>
-            <div id="wrappingImageArea" className="wrappingImage">
-                <div id="imageArea" className="imageMap" ref={imageRef}>
+            <div id="preventOnClick">
+
+            </div>
+            <div id="wrappingImageArea" className="wrappingImage" onMouseEnter={buttonAppear} onMouseLeave={buttonDisappear}>
+                <div id="imageArea" className="imageMap" ref={imageRef} style={{transform:`translate(-${imgLoc-2}00%)`}}>
                     {clothMap.map((img,i)=> 
-                    <img src={img} key={i} style={{width:'100%', height:'100%'}} onClick={productDetail}/>
+                    <img src={img} key={i} style={{width:'100%', height:'100%', float:'left'}} onClick={productDetail}/>
                     )}
                 </div>
+                <BsFillArrowLeftSquareFill id="prevBtn" style={{cursor:'pointer'}} onClick={prevBtnClick}/>
+                <BsFillArrowRightSquareFill id="nextBtn" style={{cursor:'pointer'}} onClick={nextBtnClick}/>
             </div>
             <div id="landingAboutUs">
                 
