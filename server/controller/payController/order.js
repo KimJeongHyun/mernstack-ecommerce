ddda8bf49608be34e25d9094a99f882011b7b227
KimJeongHyun <ptr1129@hanmail.net>
2021-11-18 00:11:11 +0900

    Update server order.js & Add orderSchema for transfer date to pay popup with DB solution.

--- server/controller/payController/order.js
+++ server/controller/payController/order.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const encryptSha256 = require('../../libs/encrypt')
 const config = require('../../config/config.json')
+const {orders} = require('../../model/orderSchema')
+const crypto = require('crypto');
 
 router.post('/api/getOrder',(req,res)=>{
     const reqData = req.body;
@@ -17,77 +19,102 @@ router.post('/api/getOrder',(req,res)=>{
     const host = config.host;
 
     const timestamp = new Date().getTime();
-    const orderId = 'test'
-    
-    const str = goodName+'_'+buyertel+'_'+buyerEmail+'_'+accum+'_'+couponID+'_'+couponVolume+'_'+price
 
-    const dataset = {
-        version: '1.0',
-        gopaymethod: 'VBank',
-        mid: 'INIpayTest',
-        signature: encryptSha256(`oid=${str}&price=${price}&timestamp=${timestamp}`),
-        mKey: encryptSha256('SU5JTElURV9UUklQTEVERVNfS0VZU1RS'), // 개발용, 배포용에서는 발급된 key를 사용
-        price:price,
-        oid: str,
-        timestamp:timestamp,
-        currency: 'WON',
-        goodname: goodName,
-        buyername: userID,
-        buyertel: buyertel,
-        buyeremail: buyerEmail,
-        returnUrl: host+`/api/v1/inicis/pay/after`,
-        payViewType: 'popup',
-        popupUrl: host + `/api/v1/inicis/popup/open/${str}`,
-        closeUrl: '',
+    let orderId=Date.now()+'_'+crypto.randomBytes(8).toString('hex')
+    
+    const tempTTL = new Date(Date.now()+60*1000)
+    
+    const saveOrder = () =>{
+        const orderInfo = new orders({
+            orderID:orderId,
+            userID:userID,
+            goodname:goodName,
+            buyername:userID,
+            buyertel:parseInt(buyertel),
+            buyeremail:buyerEmail,
+            usedAccum:accum,
+            usedCoupon:couponVolume,
+            couponID:couponID,
+            totalPrice:price,
+            expiredAt:tempTTL
+        })
+        orderInfo.save(function(err,result){
+            if(err) console.log(err);
+        })
     }
 
-    // 여기서 임시로 DB에 data를 저장한다.
-    // confirm을 false로 한다.
-    // TTL을 timestamp 기준 10분 뒤로 expiredDate를 지정한다.
-    return res.send({ status: 'success', data: dataset });
+    const totalWork = async () =>{
+        await saveOrder();
+        const dataset = {
+            version: '1.0',
+            gopaymethod: 'VBank',
+            mid: 'INIpayTest',
+            signature: encryptSha256(`oid=${orderId}&price=${price}&timestamp=${timestamp}`),
+            mKey: encryptSha256('SU5JTElURV9UUklQTEVERVNfS0VZU1RS'), // 개발용, 배포용에서는 발급된 key를 사용
+            price:price,
+            oid: orderId,
+            timestamp:timestamp,
+            currency: 'WON',
+            goodname: goodName,
+            buyername: userID,
+            buyertel: buyertel,
+            buyeremail: buyerEmail,
+            returnUrl: host+`/api/v1/inicis/pay/after`,
+            payViewType: 'popup',
+            popupUrl: host + `/api/v1/inicis/popup/open/${orderId}`,
+            closeUrl: '',
+        }
+        res.send({ status: 'success', data: dataset });
+    }
+    totalWork();
 })
 
-const openInicisModule = (req,res) =>{
-    const {orderParam} = req.params;
-    const splitParam = orderParam.split('_');
-    const orderId='TEST'
-    
-    const goodname = splitParam[0];
-    const buyername = req.session.userID;
-    const buyertel = splitParam[1];
-    const buyeremail = splitParam[2];
-    const accum = splitParam[3];
-    const couponID = splitParam[4];
-    const couponVolume = splitParam[5];
-    const price = parseInt(splitParam[6])
+const getOrder = (orderId) =>{
+    let result = '';
+    orders.findOne({orderID:orderId}, function(err,order){
+        result = order;
+    })
+    return result;
+}
 
+const openInicisModule = (req,res) =>{
+    const {orderId} = req.params;
 
     const timestamp = new Date().getTime();
     const host = config.host;
+
+    orders.findOne({orderID:orderId},function(err,order){
+        const goodname = order.goodname;
+        const buyername = order.buyername;
+        const buyertel = order.buyertel;
+        const buyeremail = order.buyeremail;
+        const price = order.totalPrice;
+        const dataset = {
+            version: '1.0',
+            gopaymethod: 'VBank',
+            mid: 'INIpayTest',
+            signature: encryptSha256(`oid=${orderId}&price=${price}&timestamp=${timestamp}`),
+            mKey: encryptSha256('SU5JTElURV9UUklQTEVERVNfS0VZU1RS'), // 개발용, 배포용에서는 발급된 key를 사용
+            price:price,
+            oid: orderId,
+            timestamp:timestamp,
+            currency: 'WON',
+            goodname: goodname,
+            buyername: buyername,
+            buyertel: buyertel,
+            buyeremail: buyeremail,
+            returnUrl: host+`/api/v1/inicis/pay/after`,
+            payViewType: 'popup',
+            popupUrl: host + `/api/v1/inicis/popup/open/${orderId}`,
+            closeUrl: '',
+        }
+        res.render('w_inicis', { ...dataset });
+        
+    })
     
-    const dataset = {
-        version: '1.0',
-        gopaymethod: 'VBank',
-        mid: 'INIpayTest',
-        signature: encryptSha256(`oid=${orderId}&price=${price}&timestamp=${timestamp}`),
-        mKey: encryptSha256('SU5JTElURV9UUklQTEVERVNfS0VZU1RS'), // 개발용, 배포용에서는 발급된 key를 사용
-        price:price,
-        oid: orderId,
-        timestamp:timestamp,
-        currency: 'WON',
-        goodname: goodname,
-        buyername: buyername,
-        buyertel: buyertel,
-        buyeremail: buyeremail,
-        returnUrl: host+`/api/v1/inicis/pay/after`,
-        payViewType: 'popup',
-        popupUrl: host + `/api/v1/inicis/popup/open/${orderId}`,
-        closeUrl: '',
-    }
-    res.render('w_inicis', { ...dataset });
 }
 
-router.get('/api/v1/inicis/popup/open/:orderParam',(req,res)=>{
+router.get('/api/v1/inicis/popup/open/:orderId',(req,res)=>{
     return openInicisModule(req,res);
 })
 
@@ -99,7 +126,7 @@ const onSavePaymentInfo = async (req, res) => {
         // 결제 관련 데이터 처리
         // 여기서 DB confirm을 true로 돌린다.
         // TTL 범위를 현재 시각으로부터 1년 뒤로 지정한다.
-        // 약관에 구매내역은 1년 유지된다고 올려야한다.
+        // 약관에 구매내역은 1년 유지된다고 올려야한다. 
         // orderNumber로 DB 조회하면 된다.
         return res.redirect('http://localhost:3000' + `/payment/close`);
     } else {
---
+++ server/model/orderSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    orderID:String,
+    userID:String,
+    goodname:String,
+    buyername:String,
+    buyertel:Number,
+    buyeremail:String,
+    usedAccum:Number,
+    usedCoupon:Number,
+    couponID:String,
+    totalPrice:Number,
+
+    confirmWhether:{
+        type:Boolean,
+        default:false
+    },
+    refundWhether:{
+        type:Boolean,
+        default:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    expiredAt:{
+        type:Date,
+        expires:0
+    }
+})
+
+const orders = mongoose.model('orders',orderSchema);
+
+
+module.exports={orders}
